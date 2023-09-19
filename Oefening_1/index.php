0548fd0d8d906a653f93a5f2f70b054acd971539
gdmgent-siebedegelas-2004 <siebe.degelas@student.arteveldehs.be>
2023-09-19 16:56:07 +0200

    les gedaan

--- Oefening_1/index.php
+++ Oefening_1/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">  
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Regenboog</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
     
@@ -12,7 +12,7 @@
 <div class="rainbow">
 <?php
 for ( $hue = 1; $hue <= 360; $hue++ ) {
-    echo '<div class="block>'. $hue . '</div>';
+    echo '<div class="block" style="background-color: hsl('. $hue . ', 100%, 50%);">'. $hue . '</div>';
 }
 ?>
 </div>
