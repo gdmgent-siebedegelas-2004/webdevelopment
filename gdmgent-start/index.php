14d9fa2256fce308b15c4cbe9b677f41a75670a7
gdmgent-siebedegelas-2004 <siebe.degelas@student.arteveldehs.be>
2023-09-26 16:24:16 +0200

    shows up

---
+++ gdmgent-start/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GDM.gent cases</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <?php include_once 'includes/db.php';
+    // print_r($db);
+    ?>
+<div class="projects">
+    <div class="project">
+        <h2>Project 1</h2>
+        <img src="">
+        <a href="detail.php">Lees meer ... </a>
+    </div>
+    <div class="project">
+        <h2>Project 2</h2>
+        <img src="">
+        <a href="detail.php">Lees meer ... </a>
+    </div>
+    <div class="project">
+        <h2>Project 3</h2>
+        <img src="">
+        <a href="detail.php">Lees meer ... </a>
+    </div>
+</div>
+</body>
+</html>
