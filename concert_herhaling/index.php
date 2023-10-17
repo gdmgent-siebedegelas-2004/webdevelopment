63fbb3c647a264b89a0e81e69eae640f388e1905
gdmgent-siebedegelas-2004 <siebe.degelas@student.arteveldehs.be>
2023-10-17 16:01:34 +0200

    update php concerts

--- concert_herhaling/index.php
+++ concert_herhaling/index.php
@@ -1,17 +1,45 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
+    <title>Concerts</title>
 </head>
-<h1>Concerten</h1>
-<?php
-include 'data/concerts.php';
-// print_r($concerts);
+<body>
+    <h1>Concerten</h1>
+    <?php
+    //include 'data/concerts.php';
+    //print_r($concerts);
+    include 'includes/db.php';
 
-foreach( $concerts as $key => $item) {
-    // print_r($item);  
-    // echo '<div>' . $item['artist'] . '</div>';
-    include 'views/concert_item.php';
-};
+    $sql = "SELECT concerts.*, artists.name AS artist, locations.name as location FROM concerts
+    JOIN artists ON concerts.artist_id = artists.artist_id
+    JOIN locations ON concerts.location_id = locations.location_id";
+
+    $statement = $db->prepare($sql);
+    //bind
+    $statement->execute();
+    $concerts = $statement->fetchAll();
+
+    foreach( $concerts as $key => $item ) {
+        //echo '<div>' . $item['artist'] . '</div>';
+        include 'views/concert_item.php';
+    } 
+    ?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- concert_herhaling/views/concert_item.php
+++ concert_herhaling/views/concert_item.php
@@ -3,8 +3,8 @@ print_r($item)
 ?> -->
 
 <div class="concert">
-    <h2><?= $item['concert'] ?></h2>
+    <h2><?= $item['title'] ?></h2>
     <p><?= $item['artist'] ?></p>
-    <a href="detail.php?q_id=<?= $key; ?>">meer info...</a>
+    <a href="detail.php?q_id=<?= $item['concert_id']; ?>">meer info...</a>
     <hr>
 </div>
